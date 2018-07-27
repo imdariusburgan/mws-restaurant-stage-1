dfeed582f5abae81a29dba27cb79993ec94ab253
Darius Burgan <darius@dariusburgan.com>
2018-07-26 22:32:09 -0400

    Refactor code

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,46 +1,28 @@
 const cacheName = 'v1';
 const cacheFiles = [
-    './index.html',
-    './restaurant.html',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
-    './css/styles.css',
-    './css/media-queries.css'
+    '/index.html',
+    '/restaurant.html',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/css/styles.css',
+    '/css/media-queries.css'
 ];
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            return cache.addAll([
-                '/index.html',
-                '/restaurant.html',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/img/1.jpg',
-                '/img/2.jpg',
-                '/img/3.jpg',
-                '/img/4.jpg',
-                '/img/5.jpg',
-                '/img/6.jpg',
-                '/img/7.jpg',
-                '/img/8.jpg',
-                '/img/9.jpg',
-                '/img/10.jpg',
-                '/css/styles.css',
-                '/css/media-queries.css'
-            ]);
+            return cache.addAll(cacheFiles);
         })
     );
 });
