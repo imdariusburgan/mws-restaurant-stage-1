76c7a7f1ee0078a29f228a2dd27260e7fd042b99
Darius Burgan <darius@dariusburgan.com>
2018-07-26 22:30:59 -0400

    Cache resource files

---
+++ js/service-worker.js
@@ -0,0 +1,54 @@
+const cacheName = 'v1';
+const cacheFiles = [
+    './index.html',
+    './restaurant.html',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './css/styles.css',
+    './css/media-queries.css'
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                '/index.html',
+                '/restaurant.html',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/css/styles.css',
+                '/css/media-queries.css'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[service worker] is activated correctly')
+})
+
+self.addEventListener('fetch', function(e) {
+    console.log('[service worker] fetching', e.request.url)
+})
