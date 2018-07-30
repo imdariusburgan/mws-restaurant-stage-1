9897d025516c888d26a38583e8a210da7d5b5654
Darius Burgan <darius@dariusburgan.com>
2018-07-30 19:17:29 -0400

    Cache root page

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,45 +1,47 @@
-const cacheName = 'v1';
+const cacheName = "v1";
 const cacheFiles = [
-    '/index.html',
-    '/restaurant.html',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/css/styles.css',
-    '/css/media-queries.css'
+  "/",
+  "/index.html",
+  "/restaurant.html",
+  "/js/dbhelper.js",
+  "/js/main.js",
+  "/js/restaurant_info.js",
+  "/img/1.jpg",
+  "/img/2.jpg",
+  "/img/3.jpg",
+  "/img/4.jpg",
+  "/img/5.jpg",
+  "/img/6.jpg",
+  "/img/7.jpg",
+  "/img/8.jpg",
+  "/img/9.jpg",
+  "/img/10.jpg",
+  "/css/styles.css",
+  "/css/media-queries.css"
 ];
 
-self.addEventListener('install', function(e) {
-
-    // Store files in cache
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(cacheFiles);
-        })
-    );
+self.addEventListener("install", function(e) {
+  // Store files in cache
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('[service worker] is activated correctly')
+self.addEventListener("activate", function(e) {
+  console.log("[service worker] is activated correctly");
 
-    // Remove old caches
-    e.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(thisCacheName) {
-                if (thisCacheName !== cacheName) {
-                    return caches.delete(thisCacheName);
-                }
-            }))
+  // Remove old caches
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(thisCacheName) {
+          if (thisCacheName !== cacheName) {
+            return caches.delete(thisCacheName);
+          }
         })
-    )
-})
+      );
+    })
+  );
+});
